0c658005dbda2ee8ff0aad4d48b3366c54379119
qmao002 <qizhong.mao@email.ucr.edu>
2019-06-12 11:55:08 -0700

    Init STR merge for RTree

--- hyracks-fullstack/hyracks/hyracks-storage-am-lsm-rtree/src/main/java/org/apache/hyracks/storage/am/lsm/rtree/impls/LSMRTreeLevelMergePolicyHelper.java
+++ hyracks-fullstack/hyracks/hyracks-storage-am-lsm-rtree/src/main/java/org/apache/hyracks/storage/am/lsm/rtree/impls/LSMRTreeLevelMergePolicyHelper.java
@@ -294,7 +294,299 @@ public class LSMRTreeLevelMergePolicyHelper extends AbstractLevelMergePolicyHelp
         return newComponents;
     }
 
+    private List<ILSMDiskComponent> doSTROrderMerge(ILSMIOOperation operation) throws HyracksDataException {
+        LSMRTreeMergeOperation mergeOp = (LSMRTreeMergeOperation) operation;
+        IIndexCursor cursor = mergeOp.getCursor();
+        ISearchPredicate rtreeSearchPred = new SearchPredicate(null, null);
+        ILSMIndexOperationContext opCtx = ((LSMIndexSearchCursor) cursor).getOpCtx();
+        lsmRTree.search(opCtx, cursor, rtreeSearchPred);
+
+        List<ILSMDiskComponent> newComponents = new ArrayList<>();
+        List<ILSMDiskComponentBulkLoader> componentBulkLoaders = new ArrayList<>();
+        List<ITupleReference> minTuples = new ArrayList<>();
+        List<ITupleReference> maxTuples = new ArrayList<>();
+
+        List<ILSMComponent> mergedComponents = mergeOp.getMergingComponents();
+
+        if (mergedComponents.size() == 1) {
+            ILSMDiskComponent newComponent = null;
+            ILSMDiskComponentBulkLoader componentBulkLoader = null;
+            LSMComponentFileReferences refs = lsmRTree
+                    .getNextMergeFileReferencesAtLevel(((ILSMDiskComponent) mergedComponents.get(0)).getLevel() + 1, 1);
+            newComponent = lsmRTree.createDiskComponent(refs.getInsertIndexFileReference(), null, null, true);
+            componentBulkLoader = newComponent.createBulkLoader(operation, 1.0f, false, 0L, false, false, false);
+            componentBulkLoaders.add(componentBulkLoader);
+            double[] minMBR = null;
+            double[] maxMBR = null;
+            try {
+                while (cursor.hasNext()) {
+                    cursor.next();
+                    ITupleReference frameTuple = cursor.getTuple();
+                    componentBulkLoader.add(frameTuple);
+                    double[] mbr = lsmRTree.getMBRFromTuple(frameTuple);
+                    int dim = mbr.length / 2;
+                    if (minMBR == null) {
+                        minMBR = new double[dim];
+                        System.arraycopy(mbr, 0, minMBR, 0, dim);
+                    } else {
+                        for (int i = 0; i < dim; i++) {
+                            if (mbr[i] < minMBR[i]) {
+                                minMBR[i] = mbr[i];
+                            }
+                        }
+                    }
+                    if (maxMBR == null) {
+                        maxMBR = new double[dim];
+                        System.arraycopy(mbr, dim, maxMBR, 0, dim);
+                    } else {
+                        for (int i = 0; i < dim; i++) {
+                            if (mbr[dim + i] > maxMBR[i]) {
+                                maxMBR[i] = mbr[dim + i];
+                            }
+                        }
+                    }
+                }
+            } finally {
+                cursor.close();
+            }
+            newComponent.setMinKey(AbstractLSMRTree.doublesToBytes(minMBR));
+            newComponent.setMaxKey(AbstractLSMRTree.doublesToBytes(maxMBR));
+            newComponents.add(newComponent);
+            if (newComponent.getLSMComponentFilter() != null) {
+                ITupleReference minTuple = mergedComponents.get(0).getLSMComponentFilter().getMinTuple();
+                ITupleReference maxTuple = mergedComponents.get(0).getLSMComponentFilter().getMaxTuple();
+                minTuples.add(minTuple);
+                maxTuples.add(maxTuple);
+            }
+        } else {
+            ILSMDiskComponent newComponent = null;
+            ILSMDiskComponentBulkLoader componentBulkLoader = null;
+            double[] minMBR = null;
+            double[] maxMBR = null;
+            ITupleReference minTuple = null;
+            ITupleReference maxTuple = null;
+            MultiComparator filterCmp = null;
+            long levelTo = ((ILSMDiskComponent) mergedComponents.get(0)).getLevel() + 1;
+            long start = lsmRTree.getMaxLevelId(levelTo) + 1;
+            List<FileReference> mergeFileTargets = new ArrayList<>();
+            List<FileReference> mergeBloomFilterTargets = new ArrayList<>();
+            try {
+                while (cursor.hasNext()) {
+                    cursor.next();
+                    ITupleReference frameTuple = cursor.getTuple();
+                    double[] mbr = lsmRTree.getMBRFromTuple(frameTuple);
+                    int dim = mbr.length / 2;
+                    if (minMBR == null) {
+                        minMBR = new double[dim];
+                        System.arraycopy(mbr, 0, minMBR, 0, dim);
+                    } else {
+                        for (int i = 0; i < dim; i++) {
+                            if (mbr[i] < minMBR[i]) {
+                                minMBR[i] = mbr[i];
+                            }
+                        }
+                    }
+                    if (maxMBR == null) {
+                        maxMBR = new double[dim];
+                        System.arraycopy(mbr, dim, maxMBR, 0, dim);
+                    } else {
+                        for (int i = 0; i < dim; i++) {
+                            if (mbr[dim + i] > maxMBR[i]) {
+                                maxMBR[i] = mbr[dim + i];
+                            }
+                        }
+                    }
+                    if (newComponent == null) {
+                        LSMComponentFileReferences refs = lsmRTree.getNextMergeFileReferencesAtLevel(levelTo, start++);
+                        mergeFileTargets.add(refs.getInsertIndexFileReference());
+                        mergeBloomFilterTargets.add(refs.getBloomFilterFileReference());
+                        newComponent = lsmRTree.createDiskComponent(refs.getInsertIndexFileReference(), null,
+                                refs.getBloomFilterFileReference(), true);
+                        componentBulkLoader =
+                                newComponent.createBulkLoader(operation, 1.0f, false, 0L, false, false, false);
+                        newComponents.add(newComponent);
+                        componentBulkLoaders.add(componentBulkLoader);
+                        filterCmp = newComponent.getLSMComponentFilter() == null ? null
+                                : MultiComparator.create(newComponent.getLSMComponentFilter().getFilterCmpFactories());
+                        minTuple = null;
+                        maxTuple = null;
+                    }
+                    if (minTuple == null) {
+                        minTuple = frameTuple;
+                    } else {
+                        if (filterCmp != null && filterCmp.compare(frameTuple, minTuple) < 0) {
+                            minTuple = frameTuple;
+                        }
+                    }
+                    if (maxTuple == null) {
+                        maxTuple = frameTuple;
+                    } else {
+                        if (filterCmp != null && filterCmp.compare(frameTuple, maxTuple) > 0) {
+                            maxTuple = frameTuple;
+                        }
+                    }
+                    componentBulkLoader.add(frameTuple);
+                    if (newComponent.getComponentSize() >= lsmRTree.memTableSize) {
+                        newComponent.setMinKey(AbstractLSMRTree.doublesToBytes(minMBR));
+                        newComponent.setMaxKey(AbstractLSMRTree.doublesToBytes(maxMBR));
+                        minTuples.add(minTuple);
+                        maxTuples.add(maxTuple);
+                        newComponent = null;
+                        componentBulkLoader = null;
+                        minTuple = null;
+                        maxTuple = null;
+                        filterCmp = null;
+                        minMBR = null;
+                        maxMBR = null;
+                    }
+                }
+                if (newComponent != null) {
+                    newComponent.setMinKey(AbstractLSMRTree.doublesToBytes(minMBR));
+                    newComponent.setMaxKey(AbstractLSMRTree.doublesToBytes(maxMBR));
+                    minTuples.add(minTuple);
+                    maxTuples.add(maxTuple);
+                }
+                mergeOp.setTargets(mergeFileTargets);
+                mergeOp.setBloomFilterTargets(mergeBloomFilterTargets);
+            } finally {
+                cursor.close();
+            }
+        }
+        for (int i = 0; i < newComponents.size(); i++) {
+            ILSMDiskComponent newComponent = newComponents.get(i);
+            if (newComponent.getLSMComponentFilter() != null) {
+                List<ITupleReference> filterTuples = Arrays.asList(minTuples.get(i), maxTuples.get(i));
+                lsmRTree.getFilterManager().updateFilter(newComponent.getLSMComponentFilter(), filterTuples,
+                        NoOpOperationCallback.INSTANCE);
+                lsmRTree.getFilterManager().writeFilter(newComponent.getLSMComponentFilter(),
+                        newComponent.getMetadataHolder());
+            }
+        }
+        for (ILSMDiskComponentBulkLoader componentBulkLoader : componentBulkLoaders) {
+            componentBulkLoader.end();
+        }
+
+        return newComponents;
+    }
+
     public List<ILSMDiskComponent> merge(ILSMIOOperation operation) throws HyracksDataException {
         return doZOrderMerge(operation);
     }
+
+    public static List<TuplesWithMBR> partitionTuplesBySTR(List<TupleWithMBR> tuples, long partitionTuples) {
+        if (tuples == null) {
+            return Collections.emptyList();
+        }
+        if (tuples.size() <= partitionTuples) {
+            return Collections.singletonList(new TuplesWithMBR(tuples));
+        }
+
+        List<TuplesWithMBR> partitionedTuples = new ArrayList<>();
+        // to do
+        return partitionedTuples;
+    }
+
+    static class TupleWithMBR {
+        private ITupleReference tuple;
+        private double[] mbr;
+        private int dim;
+        private double[] center;
+
+        public TupleWithMBR(ITupleReference tuple, double[] mbr) {
+            this.tuple = tuple;
+            this.mbr = mbr;
+            dim = mbr.length / 2;
+            center = new double[dim];
+            for (int i=0; i<dim; i++) {
+                center[i] = (mbr[i] + mbr[i + dim]) / 2;
+            }
+        }
+
+        public ITupleReference getTuple() {
+            return tuple;
+        }
+
+        public double[] getMBR() {
+            return mbr;
+        }
+
+        public int getDim() {
+            return dim;
+        }
+
+        public double[] getCenter() {
+            return center;
+        }
+    }
+
+    static class TuplesWithMBR {
+        private List<ITupleReference> tuples;
+        private double[] mbr = null;
+        private int dim;
+
+        public TuplesWithMBR(int dim) {
+            this.tuples = new ArrayList<>();
+            this.mbr = new double[dim*2];
+            this.dim = dim;
+        }
+
+        public TuplesWithMBR(List<ITupleReference> tuples, double[] mbr) {
+            this.tuples = new ArrayList<>(tuples);
+            this.mbr = mbr;
+            dim = mbr.length / 2;
+        }
+
+        public TuplesWithMBR(List<TupleWithMBR> tuples) {
+            this.tuples = new ArrayList<>();
+            for (TupleWithMBR tuple : tuples) {
+                double[] tupleMBR = tuple.getMBR();
+                if (mbr == null) {
+                    mbr = tupleMBR;
+                    dim = tupleMBR.length / 2;
+                } else {
+                    for (int i=0; i<dim; i++) {
+                        if (tupleMBR[i] < mbr[i]) {
+                            mbr[i] = tupleMBR[i];
+                        }
+                    }
+                    for (int i=dim; i<dim*2; i++) {
+                        if (tupleMBR[i] > mbr[i]) {
+                            mbr[i] = tupleMBR[i];
+                        }
+                    }
+                }
+                this.tuples.add(tuple.getTuple());
+            }
+            dim = mbr.length / 2;
+        }
+
+        public List<ITupleReference> getTuples() {
+            return tuples;
+        }
+
+        public double[] getMBR() {
+            return mbr;
+        }
+
+        public int getDim() {
+            return dim;
+        }
+
+        public void addTuple(TupleWithMBR tuple) {
+            tuples.add(tuple.getTuple());
+            double[] tupleMBR = tuple.getMBR();
+            for (int i=0; i<dim; i++) {
+                if (tupleMBR[i] < mbr[i]) {
+                    mbr[i] = tupleMBR[i];
+                }
+            }
+            for (int i=dim; i<dim*2; i++) {
+                if (tupleMBR[i] > mbr[i]) {
+                    mbr[i] = tupleMBR[i];
+                }
+            }
+        }
+    }
+
 }
+
